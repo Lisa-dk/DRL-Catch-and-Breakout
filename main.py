91e8b3227c3d4c5e3c5723cafa4dc414768a76fd
Mantas Majeris <m.majeris@student.rug.nl>
2023-04-28 15:48:38 +0200

    added a bit of a trianing loop (not done yet by any means)

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from catch import CatchEnv
 from policy import Policy
+from collections import deque
+import numpy as np
+import copy
 
-def train(env, episodes=500):
+BUFFER_SIZE = 2048
+
+def train(env, model, episodes=500, use_prev_state=True):
+    rewards = []
+    mem_buffer = deque(maxlen=BUFFER_SIZE)
 
     for episode in range(episodes):
-        pass
+        state, reward, done = env.reset()
+        # TODO: test if this is necessary
+        if use_prev_state:
+            prev_state = copy.deepcopy(state)
+
+        while not done:
+            action = model.policy(state)
+            if use_prev_state:
+                prev_state = copy.deepcopy(state)
+            state, reward, done = env.step(action)
+
+            mem_buffer.append((state, action, r))
+
     
 
 def main():
