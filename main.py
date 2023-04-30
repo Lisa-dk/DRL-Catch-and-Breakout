ca6cdea1597476ec22ba1522d894ec22e4c042c0
Mantas Majeris <m.majeris@student.rug.nl>
2023-04-30 14:39:30 +0200

    sort of finished the main training loop

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 from catch import CatchEnv
 from policy import Policy
 from collections import deque
+from DQN import DQN
 import numpy as np
 import copy
+import sys
 
 BUFFER_SIZE = 2048
+BATCH_SIZE = 64
 
-def train(env, model, episodes=500, use_prev_state=True):
+def train(env, model, episodes=500):
     rewards = []
     mem_buffer = deque(maxlen=BUFFER_SIZE)
 
     for episode in range(episodes):
         state, reward, done = env.reset()
-        # TODO: test if this is necessary
-        if use_prev_state:
-            prev_state = copy.deepcopy(state)
 
         while not done:
-            action = model.policy(state)
-            if use_prev_state:
-                prev_state = copy.deepcopy(state)
-            state, reward, done = env.step(action)
+            action = model.get_action(state)
+            next_state, reward, done = env.step(action)
+            rewards.append(reward)
 
-            mem_buffer.append((state, action, r))
+            mem_buffer.append((state, action, next_state, reward, done))
+            state = next_state
 
-    
+            model.learn(mem_buffer, BATCH_SIZE, target_net_update=True)
+
+    return model, rewards
+
+
+
+
+
 
 def main():
     env = CatchEnv()
+    model = DQN(env.state_shape, env.get_num_actions())
     train(env)
 
 if __name__ == '__main__':
