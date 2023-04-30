a19a6342be6246055f05b28572502b9750b46d30
Mantas Majeris <m.majeris@student.rug.nl>
2023-04-30 14:47:49 +0200

    added reward printing

--- main.py
+++ main.py
@@ -15,21 +15,22 @@ def train(env, model, episodes=500):
 
     for episode in range(episodes):
         state, reward, done = env.reset()
+        cumulative_reward = 0
 
         while not done:
             action = model.get_action(state)
             next_state, reward, done = env.step(action)
-            rewards.append(reward)
+            cumulative_reward += reward
 
             mem_buffer.append((state, action, next_state, reward, done))
             state = next_state
 
             model.learn(mem_buffer, BATCH_SIZE, target_net_update=True)
 
+        print(f"Episode {episode}; reward: {cumulative_reward}")
+        rewards.append(cumulative_reward)
+
     return model, rewards
-
-
-
 
 
 
