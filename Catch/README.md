d0a3c1cd3c809c88a4862eb19603435eccc65e76
Lisa-dk <lisadkoopmans@gmail.com>
2023-06-15 12:12:08 +0200

    cleaned up catch main.py

--- Catch/README.md
+++
@@ -1,7 +0,0 @@
-# DRL
-TODO:
-1. Implement Neural network architecture.
-    - Figure out the architecture for policy gradients.
-2. Feature extraction for problem simplicfication.
-    - Position of ball and net as pixel coordinates and horizontal velocity. 
-3. Implement high level training process.
--- Catch/main.py
+++ Catch/main.py
@@ -51,7 +51,6 @@ def evaluate_model(env, model, eval_episodes=10):
     return avg_reward / eval_episodes
         
 
-
 def train_value(env, model, episodes=2500, eval_period=10):
     rewards = []
     mem_buffer = deque(maxlen=BUFFER_SIZE) 
@@ -78,7 +77,7 @@ def train_value(env, model, episodes=2500, eval_period=10):
             mem_buffer.append((state, action, next_state, reward, done))
 
             if t % iter_copy == 0:
-                print("update")
+                print("Update target network")
                 model.learn(mem_buffer, BATCH_SIZE, target_net_update=True)
             else:
                 model.learn(mem_buffer, BATCH_SIZE, target_net_update=False)
@@ -92,74 +91,15 @@ def train_value(env, model, episodes=2500, eval_period=10):
     return model, eval_scores
 
 
-def train_policy(env, model, episodes=3000, max_episode_length=1000, eval_period=10):
-    eval_scores = []
-    batch_k = 1
-    memory = deque(maxlen=BUFFER_SIZE)
-    total_iterations = 0
-    
-    for ep in range(episodes):
-        if ep % eval_period == 0:
-            eval_reward = evaluate_model(env, model)
-            eval_scores.append(eval_reward)
-            print(f"Episode: {ep}; Reward evaluation: {eval_reward}")
-        
-        iter_copy  = 750
-        t = 0
-
-        for k in range(batch_k):
-            
-            state = env.reset()
-            state = np.transpose(state, [2, 0, 1])
-            
-            done = False
-            
-            while not done and t < max_episode_length:
-                action  = model.act(np.expand_dims(state, axis=0))
-            
-                next_state, reward, done = env.step(action)
-                
-                next_state = np.transpose(next_state, [2, 0, 1])
-                memory.append((state, action, reward, next_state, done))
-
-                model.learn_value(memory, BATCH_SIZE)
-                if total_iterations % iter_copy == 0:
-                    model.update_target_network()
-
-                state = next_state
-                t += 1
-                total_iterations += 1
-
-        most_recent = list(memory)[-t:]
-
-        states = torch.Tensor(np.array([most_recent[i][0] for i in range(t)]))
-        actions = torch.Tensor(np.asarray([most_recent[i][1] for i in range(t)]))
-        rewards = torch.Tensor(np.asarray([most_recent[i][2] for i in range(t)]))
-
-        probs = model.predict(states)
-        actions = actions[:, None].long()  
-    
-        action_probs = probs.gather(1, actions)
-        
-        model.learn_policy(action_probs, rewards, t)
-
-    return model, eval_scores
-
-
 
 def main():
-    algorithm = sys.argv[1]
-    print(algorithm)
     for run in range(1, 6):
+        print("Run: ", run)
         env = CatchEnv()
         
-        if algorithm.lower() == "dqn":
-            model = DQN(env.state_shape(), env.get_num_actions())
-            model, scores = train_value(env, model, episodes=EPISODES)
-            plot_scores(scores)
-        else:
-            model = Policy(env.state_shape(), env.get_num_actions())
-            model, scores = train_policy(env, model, episodes=EPISODES)
+        model = DQN(env.state_shape(), env.get_num_actions())
+        model, scores = train_value(env, model, episodes=EPISODES)
+        plot_scores(scores)
 
         np.save("group_07_catch_rewards_tets_" + str(run) + ".npy", scores)
 
